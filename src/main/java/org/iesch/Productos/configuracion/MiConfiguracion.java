c746cc480be47dea0e40c8bf5a1a039c3a12ac36
marcos <msanchezm@iesch.org>
2023-02-07 17:45:56 +0100

    -Lanzamos una excepcion con mensaje y configuramos CORS

--- src/main/java/org/iesch/Productos/configuracion/MiConfiguracion.java
+++ src/main/java/org/iesch/Productos/configuracion/MiConfiguracion.java
@@ -3,6 +3,8 @@ package org.iesch.Productos.configuracion;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MiConfiguracion {
@@ -10,4 +12,34 @@ public class MiConfiguracion {
     public ModelMapper modelMapper(){
         return new ModelMapper();
     }
+
+    // Cutre
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer(){
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//               // WebMvcConfigurer.super.addCorsMappings(registry);
+//                registry.addMapping("/**");
+//            }
+//        };
+//    }
+
+    // Mejor
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+               // WebMvcConfigurer.super.addCorsMappings(registry);
+                registry.addMapping("/api/productoDTO/**")
+                        .allowedOrigins("http://localhost:9999")
+                        .allowedMethods("GET") // "POST", "DELETE", ...
+                        .maxAge(3600);
+
+            }
+        };
+    }
+
+
 }
